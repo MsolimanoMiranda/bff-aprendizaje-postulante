d6fef7c0ce8cca9ef92677e821d06e40563bcd4d
josecaycho_1 <josecaycho_1@hotmail.com>
2026-08-18 16:37:04 -0500

    correcion y puebas de bugs

--- src/main/java/pe/gob/oece/bff/infrastructure/client/AprendizajePostulacion/dto/encuesta/EncuestaRespuestaRequest.java
+++ src/main/java/pe/gob/oece/bff/infrastructure/client/AprendizajePostulacion/dto/encuesta/EncuestaRespuestaRequest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public record EncuestaRespuestaRequest(
         @NotNull Long idFormulario,
+        @NotNull Long idInscripcion,
         @Valid @NotEmpty List<EncuestaRespuestaDetalleRequest> respuestas
 ) {
 }
